13721cd6508b768efb59b6e1910f90da4899c4f5
hala zraiq <hala.Zraiq@gmail.com>
2024-01-12 21:52:08 +0200

    Add mpdels & Validators

---
+++ src/models/orders.ts
@@ -0,0 +1,33 @@
+import {DataTypes } from 'sequelize';
+import { sequelize } from '../app';
+
+
+export const OrderModel = sequelize.define('orders', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'users', // This should match the table name for users
+      key: 'id', // This is the column name of the referenced model
+    }
+  },
+  total: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false,
+  },
+  status: {
+    type: DataTypes.STRING(20),
+    allowNull: false,
+    defaultValue: 'pending', // Example statuses: 'pending', 'completed', 'cancelled'
+  },
+  // Additional fields 
+}, {
+  // Sequelize model options go here
+  
+});
+
---
+++ src/models/rating_review.ts
@@ -0,0 +1,43 @@
+import {  DataTypes } from 'sequelize';
+import { sequelize } from '../app';
+
+
+export const Rating_ReviewModel = sequelize.define('reviews', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'users', 
+      key: 'id', // The column in the users table that this foreign key references
+    }
+  },
+  product_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'products',
+      key: 'id', // The column in the products table that this foreign key references
+    }
+  },
+  rating: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    validate: {
+      min: 1, // Assuming a rating scale of 1 to 5
+      max: 5
+    }
+  },
+  comment: {
+    type: DataTypes.TEXT,
+    allowNull: true, // Allowing for the possibility of a rating without a comment
+  },
+  // Any other fields 
+}, {
+    // Sequelize model options go here
+
+});
