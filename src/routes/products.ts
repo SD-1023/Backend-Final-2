2619bdff72a9695d8fd4e3b3384bd123b78da867
MarwanSamamreh <marwan.sam92@gmail.com>
2024-01-21 14:46:57 +0200

    new arrivals

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -3,18 +3,19 @@ import * as productsController from "../controllers/productsController";
 import multer from "multer";
 
 const storage = multer.memoryStorage();
-const upload = multer({ 
-    storage: storage,
-    limits: {
-        fileSize:2000000 // 2Mb 
-} });
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 2000000, // 2Mb
+  },
+});
 const router = express.Router();
 
-router.get("/",productsController.getAllProducts);
-router.get("/",productsController.getNewArrivals);
-router.get("/:id",productsController.getProductById);
-router.post("/",upload.any(),productsController.createProduct);
-router.put("/:id",productsController.updateProduct);
-router.delete("/:id",productsController.deleteProduct);
+router.get("/", productsController.getAllProducts);
+router.get("/newArrivals", productsController.getNewArrivals);
+router.get("/:id", productsController.getProductById);
+router.post("/", upload.any(), productsController.createProduct);
+router.put("/:id", productsController.updateProduct);
+router.delete("/:id", productsController.deleteProduct);
 
 export default router;
